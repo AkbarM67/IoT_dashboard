ab294c051d50948ff8742ab5fb64fb80c2a4bcc6
Akbar Maulana <akbar.maulana000501@gmail.com>
2025-07-22 17:42:02 +0700

    fix

--- routes/logRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const logController = require('../controllers/logController');
-
-router.get('/', logController.getLogs);
-
-module.exports = router;
